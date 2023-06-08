261542b36376197316cf2e9ca4c6f80990ad57fd
Samuel Ventura <samuel.ventura@yeico.com>
2023-06-08 12:04:10 -0600

    Project generation.

---
+++ lib/grabakey.ex
@@ -0,0 +1,18 @@
+defmodule Grabakey do
+  @moduledoc """
+  Documentation for `Grabakey`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Grabakey.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/grabakey_test.exs
@@ -0,0 +1,8 @@
+defmodule GrabakeyTest do
+  use ExUnit.Case
+  doctest Grabakey
+
+  test "greets the world" do
+    assert Grabakey.hello() == :world
+  end
+end
